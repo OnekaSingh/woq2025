6112905a3088ee1d66b6b7824583829967422a29
Seegurk <thierry.kaldenbach@gmail.com>
2025-06-24 21:44:19 +0200

    Precompilation of Quartic Functions

--- cost_functions.py
+++ cost_functions.py
@@ -1,6 +1,8 @@
 import numpy as np
-from electronic_hamiltonian import quadratic_terms_indices, quartic_terms_indices, quadratic_terms
+from electronic_hamiltonian import quadratic_terms_indices, quartic_terms_indices, quadratic_terms, quartic_terms
+from numba import njit
 
+@njit()
 def weight(x, z) -> float:
 
     weight = np.bitwise_or(x, z).sum()
@@ -12,6 +14,19 @@ def quadratic_term_mean_weight(x: np.ndarray, z: np.ndarray) -> float:
     N = x.shape[1]
 
     x_terms, z_terms = quadratic_terms(x, z, N)
+    num_terms = x_terms.shape[0]
+
+    total_weight = weight(x=x_terms, z=z_terms)
+
+    return total_weight/num_terms
+
+
+@njit
+def quartic_term_mean_weight(x: np.ndarray, z: np.ndarray) -> float:
+
+    N = x.shape[1]
+
+    x_terms, z_terms = quartic_terms(x, z, N)
     num_terms = x_terms.shape[0]
 
     total_weight = weight(x=x_terms, z=z_terms)
